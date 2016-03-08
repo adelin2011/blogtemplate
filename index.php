55dd0694b139b9d19b31b47d3e59725a70a081ea
Arianna Delin <arianna.delin11@ufl.edu>
2016-03-07 20:14:06 -0500

    updated index file with php

--- index.php
+++ index.php
@@ -6,20 +6,27 @@
 				if (have_posts()){
 					while (have_posts()){
 						the_post();?>
-						<h3> <?php the_title (); ?></h3>
-						<?php if ( has_post_thumbnail() ) {
-               			 the_post_thumbnail('thumbnail');
-           		 		} ?>
-						<?php the_excerpt();?>
-						<a href="<?php the_permalink(); ?>" Read More... </a>						
-					<?php }//end while
+
+		<div class="row">
+			<div class="three columns">
+				<?php if ( has_post_thumbnail() ) { the_post_thumbnail('thumbnail');
+		         } ?>
+		   </div> 
+		   <div class= "nine columns">        		 		
+				<h3> <a href="<?php the_permalink(); ?>">
+					<?php
+					the_title (); ?></h3>
+				<?php the_excerpt();?>
+				<a href="<?php the_permalink(); ?>"> Read More... </a>	
+			</div> 	
+
+				<?php }//end while
 				}//end if
 				?>
-		</div>	
-		<div class="three columns"> 
-			<h4>Sidebar </h4>
-			<p>Sidebar </p>
 		</div>
+		<div class="three columns">
+			<h3> Sidebar </h3>
+		</div>	
 	</section>	
 
 <?php get_footer(); ?>
