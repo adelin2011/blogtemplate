30abc19e16fae5d2563d64a8e7db6bcfb539f65e
Arianna Delin <arianna.delin11@ufl.edu>
2016-03-21 20:43:44 -0400

    updated styling for searchbar

--- index.php
+++ index.php
@@ -1,39 +1,39 @@
 <?php get_header(); ?>
-
-	<section class="row">
-
-		<div class="container">
+	<div class="row">
+		<div class="twelve columns">
 			<figure="image" id="image">
 				<img src="img/photo1.jpg" alt="image"/>
-			</figure> 
-		</div> 	
-
-		<div class="nine columns">
-			<?php 
-				if (have_posts()){
-					while (have_posts()){
-						the_post();?>
-
-		<div class="row">
-			<div class="three columns">
-				<?php if ( has_post_thumbnail() ) { the_post_thumbnail('thumbnail');
-		         } ?>
-		   </div> 
-		   <div class= "nine columns">        		 		
-				<h3> <a href="<?php the_permalink(); ?>">
-					<?php
-					the_title (); ?></h3>
-				<?php the_excerpt();?>
-				<a href="<?php the_permalink(); ?>"> Read More... </a>	
-			</div> 	
-
-				<?php }//end while
-				}//end if
-				?>
+			</figure>
 		</div>
-		<div class="three columns">
-			<?php dynamic_sidebar('front-page') ?>
-		</div>	
-	</section>	
+	</div>
+
+<section class="row">
+			<div class="nine columns white-bg">
+				<?php 
+				if ( have_posts() ) {
+				    while ( have_posts() ) {
+				        the_post();?>
+
+					<div class="row">
+						<div class="three columns">
+							<div class="circular-image">
+								<?php if ( has_post_thumbnail() ) { the_post_thumbnail('thumbnail');} ?>
+							</div>
+						</div>
+						<div class="nine columns">
+							<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+							<?php the_excerpt();?>
+							<a href="<?php the_permalink(); ?>">Read More...</a>
+						</div>
+					</div>
+
+				  <?php  } // end while
+				} // end if
+				?>
+			</div>
+			<div class="three columns">
+				<?php dynamic_sidebar('front-page'); ?>
+			</div>
+		</section>
 
 <?php get_footer(); ?>
