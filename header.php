73e0f2c08e97771c890757a444ed3c40a95cc842
Arianna Delin <arianna.delin11@ufl.edu>
2016-03-28 10:54:58 -0400

    changed jquery link code

--- header.php
+++ header.php
@@ -9,9 +9,9 @@
 	<!-- jQuery library (served from Google) -->
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 	<!-- bxSlider Javascript file -->
-	<script src="/js/jquery.bxslider.min.js"></script>
+	<script src="<?php echo get_template_directory_uri() . '/js/jquery.bxslider.min.js'; ?>"></script>
 	<!-- bxSlider CSS file -->
-	<link href="/css/jquery.bxslider.css" rel="stylesheet" />
+	<link href="<?php echo get_template_directory_uri() . '/css/jquery.bxslider.css'; ?>" rel="stylesheet" />
 
 </head>
 <body>
